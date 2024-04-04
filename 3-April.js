144580b412d63f0eee5b33cef3e85913d162f5fb
NAnkita24 <apjagtap24@gmail.com>
2024-04-04 19:29:26 +0530

    4 April

--- 3-April.js
+++ 3-April.js
@@ -1,2 +1,6 @@
 db.data.drop()
 db.data.insertMany([{'name':'Ankita',age:22},{'name':'Omkar',age:21}])
+db.data.insertMany([{'name':'Ankita',age:22},{_id:ObjectId('660d9c5283e0ae098cf5d55c'),'name':'Omkar',age:22},{'name':'Omkar',age:21}]) 
+db.data.insertMany([{'name':'Ankita',age:22},{_id:ObjectId('660d9c5283e0ae098cf5d55c'),'name':'Omkar',age:22},{'name':'Omkar',age:21}],{ordered:false}) 
+
+db.data.findOne({'name':'Omkar'})
